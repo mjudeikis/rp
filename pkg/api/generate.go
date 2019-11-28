7642e4a5d86d167df30dcc34aec8e7f3d45d4d75
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2019-11-28 13:45:55 +0000

    generate nit

--- pkg/api/generate.go
+++ pkg/api/generate.go
@@ -1,3 +1,4 @@
+//go:generate go get github.com/alvaroloes/enumer
 //go:generate go run github.com/alvaroloes/enumer -type InstallationPhase -output zz_generated_installationphase_enumer.go
 
 package api
